1287147ff7c0db5ec73cd25e1ea21553ec30f610
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-05-10 11:44:11 +0200

    done

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -1,5 +1,18 @@
 package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
-    // Write your code here!
+    private static ThreadSafeSingleton instance;
+
+    private ThreadSafeSingleton() {
+        }
+
+    synchronized public static ThreadSafeSingleton getInstance()
+    {
+        if (instance == null)
+        {
+            // if instance is null, initialize
+            instance = new ThreadSafeSingleton();
+        }
+        return instance;
+    }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,33 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    private Properties properties;
+
+    public H2ConnectionFactory(Properties properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public Connection createConnection() throws SQLException {
+        String jdbcDriver = properties.getProperty("jdbc_driver");
+        String dbUrl = properties.getProperty("db_url");
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+
+        try {
+            Class.forName(jdbcDriver);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Failed to load JDBC driver", e);
+        }
+
+        return DriverManager.getConnection(dbUrl, user, password);
+    }
 }
 
